af5b6f3da08d7dd9aee770b92e2f9315a3eaade8
mansi <mnayak@bestpeers.com>
2024-07-10 14:18:52 +0530

    prime

--- even_odd.py
+++ even_odd.py
@@ -1,6 +1,8 @@
 number = int(input("enter no."))
+
 def odd_even(number):
     odd = sum(int(digit) for digit in str(number) if int(digit) % 2 != 0)
     even = sum(int(digit) for digit in str(number) if int(digit) % 2 == 0)
     return {"odd_sum": odd, "even_sum": even}
+
 print(odd_even(number)) 
