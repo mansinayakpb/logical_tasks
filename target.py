9c850b474225380c941577c6620482ed4ab588d6
mansinayakpb <mnayak@bestpeers.com>
2024-07-12 10:23:01 +0530

    Update target.py

--- target.py
+++ target.py
@@ -1,6 +1,6 @@
-l = [1, 2, 3, 4, 5, 6, 7, 8,]
+lists = [1, 2, 3, 4, 5, 6, 7, 8,]
 target = int(input())
-for i in l:
-    for j in l[i:]:
-        if (i + j) == target:
-            print('{},{}'.format(i,j))
+for digit in lists:
+    for num in lists[digit:]:
+        if (digit + num) == target:
+            print('{},{}'.format(digit,num))
