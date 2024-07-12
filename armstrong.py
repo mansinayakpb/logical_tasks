407f57c4b7e595b634c9952f68a5406460e74ebf
mansinayakpb <mnayak@bestpeers.com>
2024-07-12 10:16:43 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -1,20 +1,20 @@
-def armstrong_in(n):
-    length_str = len(str(n))
-    temp = n
+def armstrong_in(number):
+    length_str = len(str(number))
+    temp = number
     sum = 0
     while temp > 0:
         digit = temp % 10
         sum = sum + digit ** length_str
         temp = temp//10
 
-    return sum == n
+    return sum == number
 
 print(armstrong_in(12))
 
-def n_armstrong(n):
+def n_armstrong(number):
     armstrong_num = []
     num = 0
-    while len(armstrong_num) < n:
+    while len(armstrong_num) < number:
         if armstrong_in(num):
             armstrong_num.append(num)
         num += 1
