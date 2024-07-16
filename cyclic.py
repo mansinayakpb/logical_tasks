9fba431d2c160c1eb39c8c612f94993e2e6327d1
mansi <mnayak@bestpeers.com>
2024-07-16 11:12:54 +0530

    cyclic

--- cyclic.py
+++ cyclic.py
@@ -1,8 +1,12 @@
+# cyclic
+
 number = int(input("Enter Number:"))
+
 
 def cyclic(number):
     while number > 9:
         number = sum(int(digit) for digit in str(number))
     return number
+
 
 print(cyclic(number))
