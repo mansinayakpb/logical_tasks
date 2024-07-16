c561c932a5befad3c1d49cf971325b88fd83a326
mansi <mnayak@bestpeers.com>
2024-07-16 11:23:38 +0530

    prime

--- prime2.py
+++ prime2.py
@@ -6,13 +6,18 @@ def f_prime(numbers):
             if number % digit == 0:
                 return False
         return True
-    
+
     primes = []
     for num in numbers:
         if is_prime(num):
             primes.append(num)
     return primes
 
+
 print(f_prime([7, 9, 3, 9, 10, 11, 27]))
 print(f_prime([10007, 1009, 1007, 27, 147, 77, 1001, 70]))
-print(f_prime([1009, 10, 10, 10, 3, 33, 9, 4, 1, 61, 63, 69, 1087, 1091, 1093, 1097]))
+print(
+    f_prime(
+        [1009, 10, 10, 10, 3, 33, 9, 4, 1, 61, 63, 69, 1087, 1091, 1093, 1097]
+    )
+)
